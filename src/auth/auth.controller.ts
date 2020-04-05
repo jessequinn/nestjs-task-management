c978e6746ccd92bf227c7b038fe49373dd196a80
Jesse Quinn <me@jessequinn.info>
2020-04-05 15:35:17 -0300

    Added: user sign up

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  @UsePipes(ValidationPipe)
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+}
